7bf50d00904d6f7592204a4ec7fdcb43502862f8
Konrad <konradbieniek@gmail.com>
2023-03-26 22:32:43 +0200

    set up project from scratch

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom"
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+class App extends React.Component {
+	render() {
+		return <div>TEST</div>
+	}
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"))
