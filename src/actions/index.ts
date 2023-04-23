d63a83f715f66200af4c9dd3eef82387c874025f
Konrad <konradbieniek@gmail.com>
2023-04-23 22:31:44 +0200

    type.ts has been added

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,1 +1,21 @@
 import axios from "axios"
+import { Dispatch } from "redux"
+
+interface Todo {
+	id: number
+	title: string
+	completed: boolean
+}
+
+const url = "https://jsonplaceholder.typicode.com/todos"
+
+export const fetchTodos = () => {
+	return async (dispatch: Dispatch) => {
+		const response = await axios.get<Todo[]>(url)
+
+		dispatch({
+			type: "GET_TODOS",
+			payload: response.data,
+		})
+	}
+}
